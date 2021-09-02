8c5031837b0fd95e702be57d94ca614981f1d3d2
cullvox <cullvox@outlook.com>
2021-09-01 22:20:10 -0400

    added more menus and bugs button

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 $('.message .close')
-  .on('click', function() {
-    $(this)
-      .closest('.message')
-      .transition('fade')
-    ;
-  })
+	.on('click', function() {
+		$(this)
+			.closest('.message')
+			.transition('fade');
+	})
 ;
 
 $("#button_AboutUs").click(function() {
-    $("#sidebar_AboutUs")
-    .sidebar('setting', 'transition', 'overlay')
-    .sidebar('toggle');
+	$("#sidebar_AboutUs")
+	.sidebar('setting', 'transition', 'overlay')
+	.sidebar('toggle');
 });
+
+$("#button_Get").click(function() {
+	$("#sidebar_Get")
+	.sidebar('setting', 'transition', 'overlay')
+	.sidebar('toggle');
+});
+
+$("#button_Bugs").click(function() {
+	$("#modal_Bugs").modal('show');
+});
+
